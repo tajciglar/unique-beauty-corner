1e07ee3ec293cbf3af01f7cf5c78bd30039511c8
tajciglar <taj.brzovic@gmail.com>
2025-01-27 09:24:33 +0100

    Commiting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,53 @@ const bodoniModa = Bodoni_Moda({
   subsets: ["latin"],
 });
 
+const services = [
+    {
+      title: "Trepalnice 1/1",
+      items: [
+        { name: "Podaljševanje na novo", price: "35 €" },
+        { name: "Korekcija po dveh tednih", price: "15 €" },
+        { name: "Korekcija po treh tednih", price: "25 €" },
+      ],
+    },
+    {
+      title: "Trepalnice Volumen",
+      items: [
+        { name: "Podaljševanje na novo", price: "45 €" },
+        { name: "Korekcija po dveh tednih", price: "25 €" },
+        { name: "Korekcija po treh tednih", price: "35 €" },
+        { name: "Odstranjevanje trepalnic", price: "10 €" },
+        { name: "Lash lift", price: "40 €" },
+      ],
+    },
+    {
+      title: "Obrvi",
+      items: [
+        { name: "Urejanje obrvi", price: "10 €" },
+        { name: "Laminacija obrvi", price: "40 €" },
+      ],
+    },
+    {
+      title: "Nohti",
+      items: [
+        { name: "Permanentno lakiranje", price: "25 €" },
+        { name: "Permanentno lakiranje z barvno bazo", price: "25 €" },
+        { name: "Odstranjevanje laka", price: "5 €" },
+      ],
+    },
+    {
+      title: "Gel Nohti",
+      items: [
+        { name: "Podaljševanje nohtov", price: "40 €" },
+        { name: "Korektura geliranih nohtov", price: "30 €" },
+        { name: "Geliranje naravnih nohtov", price: "35 €" },
+        { name: "Odstranjevanje gela", price: "5 €" },
+        { name: "Poslikava", price: "5 €" },
+      ],
+    },
+  ];
+
+
 export default function Home() {
   return (
     <div className="grid grid-rows-2 min-h-screen">
@@ -17,33 +64,33 @@ export default function Home() {
           <button type="button" className="px-3 py-2 rounded-lg w-1/4">Naroči se</button>
         </div>
       </div>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-center w-3/4 flex flex-col items-center p-10">
-          <h2 className="text-3xl font-bold mb-8">Storitve</h2>
-          <div className="w-3/4 flex justify-center gap-6">
-          <h3>Izberi storitev</h3>
-            <div className="border border-gray-200 p-4 w-1/4 shadow-lg">
-              <img alt="Slika" />
-              <p>Nohti</p>
-              <button>Izberi</button>
+      <div className="min-h-screen flex justify-center items-center w-full">
+          <div className="min-h-screen text-[var(--earth-brown)] w-3/4">
+            <h1 className="text-center text-4xl font-bold my-8">Storitve</h1>
+            <div className="w-full">
+              {services.map((service) => (
+                <div
+                  key={service.title}
+                  className="border-b-1px border-[var(--warm-gray)] rounded-lg p-6 shadow-md bg-[var(--beige)]"
+                >
+                  <h2 className="text-xl font-bold mb-4 text-[var(--terracotta)]">
+                    {service.title}
+                  </h2>
+                  <ul className="space-y-2">
+                    {service.items.map((item) => (
+                      <li
+                        key={item.name}
+                        className="flex justify-between text-lg font-medium"
+                      >
+                        <span>{item.name}</span>
+                        <span>{item.price}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
             </div>
-            <div className="border border-gray-200 p-4 w-1/4 shadow-lg">
-              <img alt="Slika" />
-              <p>Nohti</p>
-              <button>Izberi</button>
-            </div>
-            <div className="border border-gray-200 p-4 w-1/4 shadow-lg">
-              <img alt="Slika" />
-              <p>Nohti</p>
-              <button>Izberi</button>
-            </div>
-            <div className="border border-gray-200 p-4 w-1/4 shadow-lg">
-              <img alt="Slika" />
-              <p>Nohti</p>
-              <button>Izberi</button>
-            </div>
-          </div>
-        </div>
+         </div>
       </div>
     </div>
   );
--- frontend/src/app/pages/bookings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BookingPage () {
-    return (
-        <div>
-            <h1>Booking Page</h1>
-        </div>
-    )
-}
