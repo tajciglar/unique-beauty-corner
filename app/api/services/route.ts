f74530ff60f8c4225f6cfbd96ea9826340c0924a
Taj <taj.brzovic@gmail.com>
2025-10-06 12:42:16 +0200

    Prisma Decimal changes

--- app/api/services/route.ts
+++ app/api/services/route.ts
@@ -1,31 +1,32 @@
 import { NextResponse } from "next/server";
 import prisma from "@lib/prisma";
+import type { ServiceCategory, Services } from "@prisma/client";
 
 export async function GET() {
-    console.log("Fetching services...");
-    try {
-        const services = await prisma.serviceCategory.findMany({
-            include: {
-                services: {
-                    include: {
-                        serviceCategory: true,
-                    },
-                },
-                    
-            },
-        });
+  console.log("Fetching services...");
+  try {
+    const services = await prisma.serviceCategory.findMany({
+      include: {
+        services: {
+          include: {
+            serviceCategory: true,
+          },
+        },
+      },
+    });
 
-        const formattedServices = services.map((category) => ({
-            ...category,
-            services: category.services.map((service) => ({
-                ...service,
-                servicePrice: Number(service.servicePrice) // Format price to two decimal places
-            }))
-        }));
-        console.log("Services fetched successfully:", formattedServices);
-        return NextResponse.json(formattedServices);
-    } catch (error) {
-        console.error('Error fetching services:', error);
-        return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
-    }
+    const formattedServices = services.map((category: ServiceCategory & { services: Services[] }) => ({
+      ...category,
+      services: category.services.map((service: Services) => ({
+        ...service,
+        servicePrice: Number(service.servicePrice),
+      })),
+    }));
+
+    console.log("Services fetched successfully");
+    return NextResponse.json(formattedServices);
+  } catch (error) {
+    console.error("Error fetching services:", error);
+    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+  }
 }
